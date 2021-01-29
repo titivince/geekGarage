25552e5bb68c0eb55bcfc958e0d25671f66b3aec
titivince <72084738+titivince@users.noreply.github.com>
2021-01-29 10:36:38 +0100

    feat form php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+if(!empty($_POST['name'])){
+    $pdo = new PDO('mysql:host=localhost;dbname=geek', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    try {
+        $name = $_POST['name'];
+        $tel = $_POST['tel'];
+        $email = $_POST['email'];
+        $center = $_POST['center'];
+        $com = $_POST['com'];
+        $sql = $pdo->query("INSERT INTO rdv (name, tel, email, center, com) 
+            VALUES
+            ('$name', '$tel', '$email', '$center', '$com')");
+    } catch (Exeception $e) {
+        echo '<script>alert("Une erreur c\'est produite. Veuiller ressayer")</script>';
+    }
+    
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,11 +41,11 @@
             <div>
                 <h2><span>Ó</span>NLINE</h2>
                 <p>Vous acceuil pour vous aider avec son expériences</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores obcaecati nisi quae consequatur! Repudiandae, fugiat non!</p>
-                <h4>DIAGNOSTIC</h4>
-                <h4>INSTALATION SOFTWARE</h4>
-                <h4>MAINTENANCE</h4>
-                <h4>REMASTER</h4>
+                <p>Pour le</p>
+                <h3>DIAGNOSTIC</h3>
+                <h3>INSTALATION SOFTWARE</h3>
+                <h3>MAINTENANCE</h3>
+                <h3>REMASTER</h3>
             </div>
             <img src="src/dev.jpg" alt="dev team">
         </div>
@@ -40,36 +58,44 @@
         <div id="map"></div>
         <!-- center info block -->
         <div id="infoGray">
-            <h4>Horaire</h4>
+            <h2>Centre Gray</h2>
+            <h3>Adresse</h3>
+            <p>6 Rue Gambetta</p>
+            <h3>Horaire</h3>
             <p>lundi à vendredi 08:30–12:00, 13:30–17:00</p>
             <p>samedi et dimanche Fermé</p>
             <p>Téléphone : 03 84 76 90 83</p>
         </div>
         <div id="infoVienne">
-            <h4>Horaire</h4>
+        <h2>Centre Vienne</h2>
+            <h3>Adresse</h3>
+            <p>30 Avenue Général Leclerc</p>
+            <h3>Horaire</h3>
             <p>lundi à vendredi 08:30–12:00, 13:30–17:00</p>
             <p>samedi et dimanche Fermé</p>
             <p>Téléphone : 03 84 76 52 44</p>
         </div>
         <div id="infoBeynost">
-            <h4>Horaire</h4>
+            <h2>Centre Beynost</h2>
+            <h3>Adresse</h3>
+            <p>110 Rue du Chat Botté</p>
+            <h3>Horaire</h3>
             <p>lundi à vendredi 08:30–12:00, 13:30–17:00</p>
             <p>samedi et dimanche Fermé</p>
             <p>Téléphone : 03 84 76 52 44</p>
         </div>
     </div>
-    
     <!-- Pop up block -->
     <div class="form-popup" id="parentForm">
       <form id="childForm" action="" method="POST" class="form-container">
         <h2>Contact</h2>
-        <label for="name">Nom / Prenom</label>
+        <label for="name">Nom / Prenom *</label>
         <input type="text" name="name" required>
 
         <label for="phone">Telephone</label>
-        <input type="tel" name="tel" maxlength="12">
+        <input type="tel" name="tel" placeholder="0123456789" maxlength="12">
 
-        <label for="email">Email</label>
+        <label for="email">Email *</label>
         <input type="email" name="email" minlength="5" required>
 
         <label for="center">Centre</label>
@@ -80,9 +106,9 @@
             <option value="beynost">Beynost</option>
         </select>
 
-        <label for="com">Commentaire</label>
+        <label for="com">Commentaire *</label>
         <textarea placeholder="Plus d'information" name="com" required></textarea>
-
+        <p>* Champ obligatiore</p>
         <button type="submit" class="btn">Envoyer</button>
       </form>
     </div>
